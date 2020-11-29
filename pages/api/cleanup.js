9d6424de09a3af641d654c93dd982e45b8683aaa
Daniel Baker <danbaker@me.com>
2020-11-29 17:16:14 +0000

    do nothing if empty

--- pages/api/cleanup.js
+++ pages/api/cleanup.js
@@ -35,10 +35,14 @@ async function fetchGraphQL(operationsDoc, operationName, variables) {
 export default async function handler(req, res) {
   const response = await fetchGraphQL(query, "NeedsRemoval", undefined)
   const ids = response.data.needs_removal.map(node => node.id)
-  await Promise.all(ids.map(async id => {
-    await fetchGraphQL(mutation, "DeleteMessage", { id })
-  }))
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ ids }))
+  if (ids.length < 1) {
+    res.end(JSON.stringify({ ids: [] }))
+  } else {
+    await Promise.all(ids.map(async id => {
+      await fetchGraphQL(mutation, "DeleteMessage", { id })
+    }))
+    res.end(JSON.stringify({ ids }))
+  }
 }
