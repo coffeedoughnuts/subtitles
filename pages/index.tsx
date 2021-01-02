d8e139a3c820f11f491bf3e288e56a166bddae38
Daniel Baker <danbaker@me.com>
2021-01-02 12:25:43 +0000

    make viewport mobile friendly

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { gql, TypedDocumentNode, useQuery } from "@apollo/client"
 import { Author } from "../components/author"
 import { Authors } from "../types/Authors"
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 const query: TypedDocumentNode<Authors> = gql`
   query Authors {
@@ -23,20 +24,25 @@ export default function Index() {
   }
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-        {data?.authors.map(author => (
-          <Author
-            key={author.id}
-            name={author.name}
-            image={author.image}
-            onClick={onClick(author.id)}
-            size="10vw"
-            minSize={80}
-            style={{ margin: '2vw' }}
-          />
-        ))}
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width" />
+      </Head>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+          {data?.authors.map(author => (
+            <Author
+              key={author.id}
+              name={author.name}
+              image={author.image}
+              onClick={onClick(author.id)}
+              size="10vw"
+              minSize={80}
+              style={{ margin: '2vw' }}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
